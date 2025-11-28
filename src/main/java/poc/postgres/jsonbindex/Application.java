a5f02598a0d3a2b182e6004ba79a3c6a24ce95bc
Denis <denis_ermakov@rgs.ru>
2025-11-28 12:10:19 +0300

    DB migration

--- src/main/java/poc/postgres/jsonbindex/Application.java
+++ src/main/java/poc/postgres/jsonbindex/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PostgresqlJsonbIndexApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(PostgresqlJsonbIndexApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
