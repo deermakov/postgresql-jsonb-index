520be3e494e09ed54fbabd1f36c50d6f6450a5cf
Denis <denis_ermakov@rgs.ru>
2025-11-28 11:07:16 +0300

    Initial commit

---
+++ src/main/java/poc/postgres/jsonbindex/PostgresqlJsonbIndexApplication.java
@@ -0,0 +1,13 @@
+package poc.postgres.jsonbindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlJsonbIndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresqlJsonbIndexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/poc/postgres/jsonbindex/PostgresqlJsonbIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.postgres.jsonbindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresqlJsonbIndexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
